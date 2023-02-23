37fd8a4e7d80f85f340da68dd2669dbf41d86c2e
Fernando Mayer <fernandomayer@gmail.com>
2023-02-23 20:21:37 +0000

    Include paths to images

---
+++ config/auto/refs.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Haigh2021"
+    "Woodworth2016"
+    "Box2005"
+    "Platt1964"
+    "Hilborn1997"
+    "Gelman2006"
+    "Gentle2004"
+    "Geweke1992"
+    "pereira2012artigos"))
+ '(or :bibtex :latex))
+
